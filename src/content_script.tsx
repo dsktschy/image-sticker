e127b72a6678f1ab64283bff6ab0af86eaec2438
dai <chamestica@gmail.com>
2021-06-21 02:43:12 +0900

    Enable tree shaking of ReactDOM

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import ContentScript from '~/components/ContentScript'
 import { StickerObjectListContextProvider } from '~/contexts/StickerObjectList'
 
@@ -12,4 +12,4 @@ const App = () => (
 const divEl = document.createElement('div')
 divEl.setAttribute('id', 'imgstckr')
 document.body.appendChild(divEl)
-ReactDOM.render(<App />, divEl)
+render(<App />, divEl)
--- src/default_popup.tsx
+++ src/default_popup.tsx
@@ -1,7 +1,7 @@
 import 'sanitize.css'
 import 'reset-css'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import DefaultPopup from '~/components/DefaultPopup'
 
 const App = () => <DefaultPopup />
@@ -9,4 +9,4 @@ const App = () => <DefaultPopup />
 const divEl = document.createElement('div')
 divEl.setAttribute('id', 'imgstckr')
 document.body.appendChild(divEl)
-ReactDOM.render(<App />, divEl)
+render(<App />, divEl)
